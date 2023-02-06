eb98d8a6453f942c28e95dfbbcaa092492c2ed1f
Camwig <2001551@uad.ac.uk>
2023-02-06 13:08:35 +0000

    Still working on the grid system
    There is progress its just taking a bit longer than I would like

--- Prototyping build/Assets/Scenes/Scripts/GridSystem.cs
+++ Prototyping build/Assets/Scenes/Scripts/GridSystem.cs
@@ -7,15 +7,29 @@ public class GridSystem
     private int width;
     private int height;
     private float cellSize;
+    private Vector3 OriginPos;
     private int[,] gridArray;
+    private TextMesh[,] debugTextArray;
 
-    public GridSystem(int width, int height, float cellSize)
+    public GridSystem(int width, int height, float cellSize, Vector3 OriginPos)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
+        this.OriginPos = OriginPos;
 
         gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        //Used to offest the grid to the upper left hand corner
+
+        //Currently the half the width and the height of the camera view port
+
+        //Base view size for the camera is 4 width and 2 height
+
+        //Shouldnt matter too much later as well have a set size we will expnad as we go later
+        int diffrence = 0;
+        int diffrence2 = 0;
 
         //Debug.Log(width + "" + height);
         for (int x =0; x < gridArray.GetLength(0); x++)
@@ -23,18 +37,43 @@ public class GridSystem
             for (int y=0; y<gridArray.GetLength(1); y++)
             {
                 //Debug.Log(x + " " + y);
-                CreateWorldText(null,gridArray[x,y].ToString(), GetWorldPosition(x,y),20,Color.white, TextAnchor.MiddleCenter, TextAlignment.Center,1);
-                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);                
-                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y),Color.white,100f);
+                //Need to move it to the camera space aka reduce the size of the grid
+                debugTextArray[x,y] = CreateWorldText(null,gridArray[x,y].ToString(), (GetWorldPosition(x - diffrence, y - diffrence2) + new Vector3(cellSize,cellSize)*0.5f),5,Color.white, TextAnchor.MiddleCenter, TextAlignment.Center,1);
+                Debug.DrawLine(GetWorldPosition((x- diffrence), (y - diffrence2)), GetWorldPosition((x - diffrence), (y - diffrence2) + 1), Color.white, 100f);                
+                Debug.DrawLine(GetWorldPosition((x - diffrence), (y - diffrence2)), GetWorldPosition((x - diffrence) + 1, (y - diffrence2)),Color.white,100f);
             }
         }
-        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width,height), Color.white, 100f);
-        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(-diffrence, height - diffrence2), GetWorldPosition(width- diffrence, height- diffrence2), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width - diffrence, -diffrence2), GetWorldPosition(width- diffrence, height- diffrence2), Color.white, 100f);
+
+        //SetValue(2, 1, 53);
     }
 
     private Vector3 GetWorldPosition(int x,int y)
     {
-        return new Vector3(x, y) * cellSize;
+        return new Vector3(x, y) * cellSize + OriginPos;
+    }
+
+    private void  GetXY(Vector3 worldPosition, out int x ,out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - OriginPos).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - OriginPos).y / cellSize);
+    }
+
+    public void SetValue(int x,int y,int value)
+    {
+        if(x >=0 && y >=0 && x < width && y <height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
+    }
+
+    public void SetThisValue(Vector3 worldPosition,int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
     }
 
     //private static TextMesh CreateWorldText(string text, Transform parent =null, Vector3 localPosition = default(Vector3),int fontSize = 40, Color color, TextAnchor textAnchor , TextAlignment textAlignment, int sortingOrder)
--- Prototyping build/Assets/Scenes/Scripts/TestScript.cs
+++ Prototyping build/Assets/Scenes/Scripts/TestScript.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    private GridSystem grid;
+
     private void Start()
     {
-        GridSystem grid = new GridSystem(4, 2, 10f);
+        /*18,10*/
+        grid = new GridSystem(18, 10, 2f, new Vector3(-9,-5));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 new_vector;
+            new_vector = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+
+            grid.SetThisValue(GetMouseWorldPosition(), 75);
+
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionZ(Input.mousePosition, Camera.main);
+        vec.z = 0.0f;
+        return vec;
+    }
+
+    private static Vector3 GetMouseWorldPositionZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
     }
 }
