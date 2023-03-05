784061333b6247346fe5e6b849add5c3072d5506
Tannoy2021 <georgigrigorov01@gmail.com>
2023-03-05 20:26:16 +0000

    yt
    test

---
+++ Assets/ScriptableObjects.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableObjects : MonoBehaviour
+{
+
+
+    public ScriptableObjects objectsType;
+    public enum LayoutObjects
+    {
+        email,
+        clock,
+        label,
+        wattage,
+        temperatureRiser,
+        OnOffSwitch,
+        ThreeWaySelector,
+        Dial
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
